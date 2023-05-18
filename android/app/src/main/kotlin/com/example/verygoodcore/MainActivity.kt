f54294e5b7612f4b240b0c0e95423ddf79551e04
Krzysztof Bury <krzysztof.bury@gamedesire.com>
2023-05-18 07:10:00 +0200

    Empty Project + mason

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.pancake
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:pancake/app/app.dart';
+import 'package:pancake/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
